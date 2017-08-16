129ab454bc25437fd5041f683879c1e931553213
leopen <790480953@qq.com>
2017-08-16 13:49:30 +0800

    [leopen] 1.add source map 2. add webpack-dev-server 3. add lodash

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,5 +1,7 @@
 import { markdown } from 'markdown'
+import test from './test'
 import './demo.scss'
+import debounce from 'lodash/debounce'
 
 const mdContent = '# Hello *Markdown.js* !'
 const htmlContent = markdown.toHTML(mdContent)
@@ -8,9 +10,17 @@ const header = document.getElementById('header')
 header.innerHTML = htmlContent
 header.classList.add('hello')
 
-export function preview () {
+test()
+
+function preview () {
   const inputContent = document.getElementById('mdInput').value
   const previewHTML = markdown.toHTML(inputContent)
   const previewer = document.getElementById('previewer')
   previewer.innerHTML = previewHTML
 }
+
+const execPreview = debounce(preview, 500)
+
+document.getElementById('mdInput').addEventListener('keypress', () => {
+  execPreview()
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/demo/demo.js',
+  entry: {
+    demo: './src/demo/demo.js',
+    test: './src/demo/test.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'test html webpack plugin',
+      template: './src/demo/demo.html'
+    })
+  ],
+  devtool: 'inline-source-map',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/demo')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist/demo'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: './dist'
   },
   module: {
     rules: [
